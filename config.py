a57e90d8b6e4dcdbf2d5dd07f958dd5c48d7cfb9
tikendraw <tikendraksahu1029@gmail.com>
2023-08-24 12:18:20 +0530

    config updated

--- config.py
+++ config.py
@@ -8,12 +8,16 @@ PREPROCESSED_DATA_PATH = Path(DATA_DIR)/ "preprocessed_df.csv"
 # Model paths
 MODEL_DIR = Path(os.getcwd())/ "model"
                  
-TEXT_VECTOR_FILENAME = "tv_layer.pkl"
+TEXT_VECTOR_FILENAME = "model/tv_layer.pkl"
 MODEL1_FILENAME = "model1.h5"
 
 # Model hyperparameters
-MAX_TOKEN = 100_000
-OUTPUT_SEQUENCE_LENGTH = 100
+MAX_TOKEN = 100_000  # don't change this
+OUTPUT_SEQUENCE_LENGTH = 175 # don't change this
 BATCH_SIZE = 64
 DIM = 8
-EPOCHS = 10
+EPOCHS = 2
+TRAIN_SIZE = 0.1
+
+#callback
+EARLY_STOPPING_PATIENCE = 2
