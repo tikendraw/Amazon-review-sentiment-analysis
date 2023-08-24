f98a3cf1a6ef8ffb2c6cf085e6b24ad8662de5ec
tikendraw <tikendraksahu1029@gmail.com>
2023-08-24 16:40:39 +0530

    moved preprocessing func to new file

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,13 @@ from tensorflow.keras.layers import TextVectorization
 import tensorflow as tf
 import logging
 import os
-
+from pathlib import Path
 
 # Configure logging
 def configure_logging(log_dir, log_filename, log_level=logging.INFO):
     
-    log_dir = Path(log_dir).mkdir(exist_ok=True)
+    log_dir = Path(log_dir)
+    log_dir.mkdir(exist_ok=True)
     log_file = log_dir / log_filename
     
     # Configure logging to both console and file
@@ -20,10 +21,6 @@ def configure_logging(log_dir, log_filename, log_level=logging.INFO):
                             logging.FileHandler(log_file)
                         ])
     
-def clean_text(x):
-    x = re.sub(r'[^\w\s]', '', x)
-    x = x.lower()
-    return x
 
 def save_text_vectorizer(text_vectorizer, filename):
     config = text_vectorizer.get_config()
@@ -38,57 +35,3 @@ def load_text_vectorizer(text_vectorizer, filename):
         text_vectorizer.set_weights(data['weights'])
         return text_vectorizer
 
-
-# src/data_preprocessing.py
-
-import re
-import polars as pl
-from pathlib import Path
-from sklearn.model_selection import train_test_split
-
-def preprocess_data(data_dir):
-    # Read the CSV file using Polars
-    df = pl.read_csv(data_dir / 'train.csv', new_columns=['polarity', 'title', 'text'])
-
-    assert df['polarity'].max()==2
-    assert df['polarity'].min()==1
-
-
-    # Drop rows with null values
-    df.drop_nulls()
-
-    # Map polarity to binary values (0 for negative, 1 for positive)
-    df = df.with_columns([
-        pl.col('polarity').apply(lambda x: 0 if x == 1 else 1)
-    ])
-
-    # Cast polarity column to Int16
-    df = df.with_columns([
-        pl.col('polarity').cast(pl.Int16, strict=False)
-    ])
-
-    # Combine title and text columns to create the review column
-    df = df.with_columns([
-        (pl.col('title') + ' ' + pl.col('text')).alias('review')
-    ])
-    
-    df = df.with_columns([
-	(pl.col('review').str().lower())
-	])
-	
-    # Select relevant columns
-    df = df.select(['review', 'polarity'])
-
-    # Perform text cleaning using a function
-    df = df.with_columns([
-        pl.col('review').apply(clean_text)
-    ])
-
-    df.write_csv(data_dir/'preprocessed_df.csv')
-    
-
-def clean_text(x: str) -> str:
-    # Remove punctuation and convert to lowercase
-    x = re.sub(r'[^\w\s]', '', x)
-    x = x.lower()
-    return x
