d2dbea89b3d3b9bd44a9f4e123c5509d8fc831b2
tikendraw <tikendraksahu1029@gmail.com>
2023-08-29 18:58:07 +0530

    predicts sentiment

--- predict.py
+++ predict.py
@@ -2,30 +2,24 @@ import os
 import logging
 import tensorflow as tf
 from pathlib import Path
-from src import utils
+from src.utils import configure_logging, load_model_and_vectorizer
+
+from src.data_preprocessing import clean_text
 import config
+from tensorflow.keras.layers import TextVectorization
 
-
-def load_model_and_vectorizer():
-    text_vectorizer = tf.keras.layers.TextVectorization(
-        max_tokens=config.MAX_TOKEN,
-        output_sequence_length=config.OUTPUT_SEQUENCE_LENGTH,
-        pad_to_max_tokens=True,
-    )
     
-    logging.info('Loading text vectorizer and model...')
-    try:
-        text_vectorizer = utils.load_text_vectorizer(text_vectorizer, config.TEXT_VECTOR_FILENAME)
-        model_path = os.path.join(config.MODEL_DIR, config.MODEL1_FILENAME)
-        lstm_model = tf.keras.models.load_model(model_path)
-        return text_vectorizer, lstm_model
-    except Exception as e:
-        logging.error(f'Error loading vectorizer and model: {e}')
-        return None, None
+# constants
+DATA_DIR = Path(os.getcwd()) / 'dataset'
+DATA_PATH = DATA_DIR / 'preprocessed_df.csv'
+MODEL_PATH = Path(config.MODEL_DIR) / config.MODEL_FILENAME
+VECTORIZER_PATH = Path(config.MODEL_DIR) / config.TEXT_VECTOR_FILENAME
+COUNTER_PATH = Path(config.MODEL_DIR) / config.COUNTER_NAME
+
 
 def predict_sentiment(title, text, text_vectorizer, lstm_model):
     review = f'{title} {text}' # concatenate the title and text
-    clean_review = utils.clean_text(review)
+    clean_review = clean_text(review)
     review_sequence = text_vectorizer([clean_review])
     prediction = lstm_model.predict(review_sequence)
     sentiment_score = prediction[0][0]
@@ -33,8 +27,8 @@ def predict_sentiment(title, text, text_vectorizer, lstm_model):
     return sentiment_label, sentiment_score
 
 def main():
-    utils.configure_logging(config.LOG_DIR, "prediction_log.txt", logging.INFO)
-    text_vectorizer, lstm_model = load_model_and_vectorizer()
+    configure_logging(config.LOG_DIR, "prediction_log.txt", logging.INFO)
+    text_vectorizer, lstm_model = load_model_and_vectorizer(VECTORIZER_PATH, MODEL_PATH)
     
     if text_vectorizer is None or lstm_model is None:
         logging.error('Could not load text vectorizer and model. Aborting prediction.')
