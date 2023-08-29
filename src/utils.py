7ea671a2ef78d3c8d1fbdf2ebd9d8205d0b5c166
tikendraw <tikendraksahu1029@gmail.com>
2023-08-29 18:58:59 +0530

    moved function

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import logging
 import os
 from pathlib import Path
-
+from .data_preprocessing import clean_text
 # Configure logging
 def configure_logging(log_dir, log_filename, log_level=logging.INFO):
     
@@ -20,18 +20,47 @@ def configure_logging(log_dir, log_filename, log_level=logging.INFO):
                             logging.StreamHandler(),
                             logging.FileHandler(log_file)
                         ])
+    return
     
 
 def save_text_vectorizer(text_vectorizer, filename):
     config = text_vectorizer.get_config()
-    weights = text_vectorizer.get_weights()
     with open(filename, 'wb') as f:
-        pickle.dump({'config': config, 'weights': weights}, f)
+        pickle.dump({'config': config}, f)
 
-def load_text_vectorizer(text_vectorizer, filename):
-    with open(filename, 'rb') as f:
-        data = pickle.load(f)
-        text_vectorizer.adapt(tf.data.Dataset.from_tensor_slices(["xyz"]))
-        text_vectorizer.set_weights(data['weights'])
-        return text_vectorizer
 
+def load_counter(filename):
+    with open (filename,'rb')  as counter :
+        return pickle.load(counter)
+    
+
+def load_model(model, model_dir):
+    """Load the model from disk."""
+    # Load the Keras model
+    return model.load_weights(model_dir)
+
+
+def load_text_vectorizer(vectorizer_path):
+    from_disk = pickle.load(open(vectorizer_path, "rb"))
+    return TextVectorization.from_config(from_disk['config'])
+
+
+def load_model_and_vectorizer(vectorizer_path, model_path):
+    try:
+        text_vectorizer = load_text_vectorizer(vectorizer_path)
+        lstm_model = tf.keras.models.load_model(model_path)
+        return text_vectorizer, lstm_model
+    except Exception as e:
+        logging.error(f'Error loading vectorizer and model: {e}')
+        return None, None
+
+
+
+def predict_sentiment(title, text, text_vectorizer, lstm_model):
+    review = f'{title} {text}' # concatenate the title and text
+    clean_review = clean_text(review)
+    review_sequence = text_vectorizer([clean_review])
+    prediction = lstm_model.predict(review_sequence)
+    sentiment_score = prediction[0][0]
+    sentiment_label = 'Positive' if sentiment_score >= 0.5 else 'Negative'
+    return sentiment_label, sentiment_score
