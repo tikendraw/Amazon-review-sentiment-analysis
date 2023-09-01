e76577c7707f420d07c1b9ff494e0aa13e3fb778
tikendraw <tikendraksahu1029@gmail.com>
2023-09-01 16:17:13 +0530

    Updated the create_datasets function to support different n_repeat values.

--- src/model.py
+++ src/model.py
@@ -1,14 +1,16 @@
 # model.py
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, TextVectorization
 from tensorflow import keras
+from tensorflow.keras.layers import (LSTM, Dense, Dropout, Embedding,
+                                     TextVectorization)
+
 
 def create_lstm_model(input_shape, max_tokens, dim):
-    inputs = keras.Input(shape=input_shape)
-    embedding_layer = Embedding(input_dim=max_tokens, output_dim=dim, mask_zero=True, input_length=input_shape[0])(inputs)
-    x = LSTM(16, return_sequences=True)(embedding_layer)
-    x = LSTM(16)(x)
-    x = Dropout(0.4)(x)
-    x = Dense(64, activation='relu')(x)
-    outputs = Dense(1, activation='sigmoid')(x)
+    inputs = keras.Input(shape=(input_shape))
+    embedding_layer = Embedding(input_dim=max_tokens, output_dim=dim, mask_zero=True, input_length=input_shape, name='embedding_layer')(inputs)
+    x = LSTM(16, return_sequences=True, name = 'lstm_layer_1')(embedding_layer)
+    x = LSTM(16, name = 'lstm_layer_2')(x)
+    x = Dropout(0.4, name ='dropout_layer')(x)
+    x = Dense(64, activation='relu', name = 'dense_layer_1')(x)
+    outputs = Dense(1, activation='sigmoid', name = 'dense_layer_2_final')(x)
     return keras.Model(inputs=inputs, outputs=outputs, name='model_lstm')
 
